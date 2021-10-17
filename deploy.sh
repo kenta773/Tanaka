6dfec3d342b49c45fdcb2bb06aacc3d41ab899ec
kenta <kenta.mizumura0819@gmail.com>
2021-10-17 22:14:13 +0900

    デプロイ方法修正

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 if [ "$1" = "temp" ];then
     aws cloudformation deploy --template template.json --stack-name Tanaka-stack --parameter-overrides BucketName=tanakamenubucket FileName=menulist.csv DynamoDBTableName=TanakaMenuDB --capabilities CAPABILITY_NAMED_IAM
 elif [ "$1" = "s3" ];then
+    aws s3 rm s3://tanakamenubucket/menulist.csv
     aws s3 sync s3 s3://tanakamenubucket/ --delete
 else
     echo 'parameter error (temp or s3)'
